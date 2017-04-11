107c0150aacb4b4f92a1b4b14e6f145b52d1b6db
marcosmarcon <mvgmarcon@gmail.com>
2017-04-11 20:36:45 -0300

    Update ilhas.java

--- ilhasTrabalho1/src/ilhas.java
+++ ilhasTrabalho1/src/ilhas.java
@@ -1,5 +1,3 @@
-
- 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,7 +5,6 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
- 
 
 public class ilhas {
 	  public static void main(String[] args) {
@@ -61,7 +58,7 @@ public class ilhas {
 		    for(int i=0; i<matrizI; i++){
 		        for(int j=0; j<matrizJ; j++){          
 		            if(matriz[i][j]== 1){           // se for igual a 1 
-		                count++;					// incrementa o count e chama o metodo
+		                count++;		    // incrementa o count e chama o metodo
 		                arquipelogo(matriz, i, j);  // passando a matriz e sua posicao atual (i,j)                 
 		            }
 		        }
@@ -73,25 +70,19 @@ public class ilhas {
 		    int matrizI = matriz.length;    // tamanho da matriz  
 		    int matrizJ = matriz[0].length; 
 
-          /*verifica se o i e o j sao maiores ou menores que o tamamho da matriz 
+                    /*verifica se o i e o j sao maiores ou menores que o tamamho da matriz 
 		    ou se a posicao atual eh diferente de 1 */ 
-		    
 		    if(i<0 || i>=matrizI || j<0 || j>=matrizJ || matriz[i][j]!= 1) 
 		       return;      // se for tudo true continua e chama o proprio metodo novamente 
 		    
 		    matriz[i][j]=0; // caso a posicao atual seja 1 no teste acima, a posicao atual recebe 0 
-		    				// evitando que uma posicao seja contada indevidamente
+		    		    // evitando que uma posicao seja contada indevidamente
 		    
 		    arquipelogo(matriz, i-1, j); // passa por parametro outras posicoes para serem testadas
-            arquipelogo(matriz, i+1, j);
-            arquipelogo(matriz, i, j-1);
-            arquipelogo(matriz, i, j+1);
+            	    arquipelogo(matriz, i+1, j);
+                    arquipelogo(matriz, i, j-1);
+            	    arquipelogo(matriz, i, j+1);
            
-
-
-
-
-
 		}	    
   
 }
